c0cb4498a09094599ddad60ef759959989da4511
AZAHAZA <tzuy3@uw.edu>
2022-03-02 23:49:08 -0800

    file update

--- source/analysis.R
+++ source/analysis.R
@@ -1,11 +1,14 @@
 library("tidyverse",warn.conflicts = FALSE)
 library("plotly",warn.conflicts = FALSE)
 library("rjson",warn.conflicts = FALSE)
+library(plotly)
+library(rjson)
 
+json <- fromJSON(file="https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json")
 #source data
+code <- read_csv("../source/incarceration-trends-master/incarceration_trends.csv",show_col_types = FALSE)
+View(code)
 df <- read_csv("../source/incarceration-trends-master/incarceration_trends.csv",show_col_types = FALSE)
-View(df)
-
 #find the total population of each year  
 total_population <- select(df, year, total_jail_pop, total_prison_pop) %>% 
   group_by(year)%>%
@@ -41,8 +44,7 @@ race_population <- select(df, year, aapi_prison_pop, black_prison_pop, latinx_pr
             sum_latinx_pop = sum(latinx_prison_pop,na.rm = TRUE) + sum(black_jail_pop,na.rm = TRUE),
             sum_native_pop = sum(native_prison_pop,na.rm = TRUE) + sum(native_jail_pop,na.rm = TRUE),
             sum_white_pop = sum(black_prison_pop,na.rm = TRUE) + sum(black_jail_pop,na.rm = TRUE) 
-  )%>% 
-
+  )
 race_population2 <- race_population %>% slice(-c(1:15, 48:49))
 
 View(race_population2)
@@ -81,13 +83,13 @@ county_map <- select(df,county_name, aapi_prison_pop, black_prison_pop, latinx_p
   )
   
 
-View(urbanicity_map)
-write.csv(urbanicity_map,'urbanicity_map.cvs')
+View(county_map)
+write.csv(county_map,'county_map.cvs')
 
 url <- 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
 counties <- rjson::fromJSON(file=url)
-url2<- 
-df <- read.csv(url2, colClasses=c(county_name="character"))
+url2<- 'https://github.com/info-201a-wi22/a3-AZAHAZA/blob/main/source/urbanicity_map.cvs'
+data <- read.csv(url2, colClasses=c(county_name="character"))
 g <- list(
   scope = 'usa',
   projection = list(type = 'albers usa'),
@@ -98,8 +100,8 @@ fig2 <- plot_ly()
 fig2 <- fig2 %>% add_trace(
   type="choropleth",
   geojson=counties,
-  locations=urbanicity_map$county_name,
-  z=urbanicity_map$total_pop,
+  locations=data$county_name,
+  z=data$total_pop,
   colorscale="Viridis",
   zmin=0,
   zmax=2535942,
