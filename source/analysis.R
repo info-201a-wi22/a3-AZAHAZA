c69f42d9d82bee84ab344ce32409d77eeb728ba4
AZAHAZA <tzuy3@uw.edu>
2022-03-04 23:38:07 -0800

    a3

--- source/analysis.R
+++ source/analysis.R
@@ -2,13 +2,12 @@ library("tidyverse",warn.conflicts = FALSE)
 library("plotly",warn.conflicts = FALSE)
 library("rjson",warn.conflicts = FALSE)
 library(plotly)
-library(rjson)
+library(usdata)
 
-json <- fromJSON(file="https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json")
+
 #source data
-code <- read_csv("../source/incarceration-trends-master/incarceration_trends.csv",show_col_types = FALSE)
-View(code)
 df <- read_csv("../source/incarceration-trends-master/incarceration_trends.csv",show_col_types = FALSE)
+View(df)
 #find the total population of each year  
 total_population <- select(df, year, total_jail_pop, total_prison_pop) %>% 
   group_by(year)%>%
@@ -19,7 +18,13 @@ total_population <- select(df, year, total_jail_pop, total_prison_pop) %>%
 total_population2 <- total_population %>% slice(-c(48,49))
 
 view(total_population2)
- 
+
+max_pop <- max(total_population2)
+min_pop <- min(total_population2)
+max_year <- max(total_population2$year)
+min_year <- min(total_population2$year)
+change <- (max_pop-min_pop)/(max_year-min_year)
+View(change)
 #create line chart of the total population increased by year 
 line_plot<- plot_ly(
   data = total_population2,     
@@ -35,15 +40,24 @@ line_plot<- plot_ly(
 
 line_plot
 
+blackwhite_population <- select(df, year, black_prison_pop, black_jail_pop,white_prison_pop,white_jail_pop )%>%
+  group_by(year)%>%
+    summarise(black_pop_total = sum(black_prison_pop,na.rm = TRUE) + sum(black_jail_pop,na.rm = TRUE),
+              sum_white_pop = sum(white_prison_pop,na.rm = TRUE) + sum(white_jail_pop,na.rm = TRUE))
+change_black_pop <- (247546.-3171.0)/(2018-1970)
+change_white_pop <- (346061.8-1666.0)/(2018-1970)
+
+
 race_population <- select(df, year, aapi_prison_pop, black_prison_pop, latinx_prison_pop,
                           native_prison_pop, white_prison_pop, aapi_jail_pop, black_jail_pop,
                           latinx_jail_pop, native_jail_pop, white_jail_pop) %>% 
   group_by(year)%>%
   summarise(sum_aapi_pop = sum(aapi_prison_pop,na.rm = TRUE) + sum(aapi_jail_pop,na.rm = TRUE) ,
-            sum_black_pop = sum(black_prison_pop,na.rm = TRUE) + sum(latinx_jail_pop,na.rm = TRUE),
-            sum_latinx_pop = sum(latinx_prison_pop,na.rm = TRUE) + sum(black_jail_pop,na.rm = TRUE),
+            sum_black_pop = sum(black_prison_pop,na.rm = TRUE) + sum(black_jail_pop,na.rm = TRUE),
+            sum_latinx_pop = sum(latinx_prison_pop,na.rm = TRUE) + sum(latinx_jail_pop,na.rm = TRUE),
             sum_native_pop = sum(native_prison_pop,na.rm = TRUE) + sum(native_jail_pop,na.rm = TRUE),
-            sum_white_pop = sum(black_prison_pop,na.rm = TRUE) + sum(black_jail_pop,na.rm = TRUE) 
+            sum_white_pop = sum(black_prison_pop,na.rm = TRUE) + sum(black_jail_pop,na.rm = TRUE),
+            Total_jail_pop = sum(sum_aapi_pop ,sum_black_pop , sum_latinx_pop , sum_native_pop ,sum_white_pop)
   )
 race_population2 <- race_population %>% slice(-c(1:15, 48:49))
 
@@ -59,7 +73,7 @@ white <- c(race_population2$sum_white_pop)
 
 data <- data.frame(race_population2,year, aapi, black, latinx, native, white)
 
-fig <- plot_ly(data, x = ~Population_Count, y = ~aapi, type = 'bar', name = 'aapi')
+fig <- plot_ly(data, x = ~race_population2, y = ~aapi, type = 'bar', name = 'aapi')
 fig <- fig %>% add_trace(y = ~black, name = 'Black')
 fig <- fig %>% add_trace(y = ~latinx, name = 'Latinx')
 fig <- fig %>% add_trace(y = ~native, name = 'Native')
@@ -68,53 +82,67 @@ fig <- fig %>% layout(yaxis = list(title = 'Count'), barmode = 'stack')
 
 fig
 
+total_pop <- select(df, year, aapi_pop_15to64, black_pop_15to64, latinx_pop_15to64, native_pop_15to64,
+                    white_pop_15to64,) %>% 
+  group_by(year)%>%
+  summarise(total_aapi_pop = sum(aapi_pop_15to64),
+            total_black_pop = sum(black_pop_15to64),
+            total_latinx_pop = sum(latinx_pop_15to64),
+            total_native_pop = sum(native_pop_15to64),
+            total_white_pop = sum(white_pop_15to64)
+  )%>% slice(-c(1:20))
+
+View(total_pop)
+
 #sort out urban and the different area code 
-county_map <- select(df,county_name, aapi_prison_pop, black_prison_pop, latinx_prison_pop,
+state_map_2016 <- filter(df, year == 2016, na.rm = TRUE)
+View(state_map_2016)
+state_map <- select(state_map_2016, state, aapi_prison_pop, black_prison_pop, latinx_prison_pop,
                          native_prison_pop, white_prison_pop, aapi_jail_pop, black_jail_pop,
                          latinx_jail_pop, native_jail_pop, white_jail_pop) %>%
-  group_by(county_name)%>%
+  
+  group_by(state)%>%
   summarise(
             sum_aapi_pop = sum(aapi_prison_pop,na.rm = TRUE) + sum(aapi_jail_pop,na.rm = TRUE) ,
             sum_black_pop = sum(black_prison_pop,na.rm = TRUE) + sum(latinx_jail_pop,na.rm = TRUE),
             sum_latinx_pop = sum(latinx_prison_pop,na.rm = TRUE) + sum(black_jail_pop,na.rm = TRUE),
             sum_native_pop = sum(native_prison_pop,na.rm = TRUE) + sum(native_jail_pop,na.rm = TRUE),
             sum_white_pop = sum(black_prison_pop,na.rm = TRUE) + sum(black_jail_pop,na.rm = TRUE),
-            total_pop = sum_aapi_pop + sum_black_pop + sum_latinx_pop + sum_native_pop + sum_white_pop
-  )
+            total_pop = sum(sum_aapi_pop ,sum_black_pop , sum_latinx_pop , sum_native_pop ,sum_white_pop)
+          )
   
+View(state_map)
+#write.csv(state_map,'state_map.csv')
+total_pop_states <- sum(state_map$total_pop)
+black_total_2016 <- sum(state_map$sum_black_pop)
+white_total_2016 <- sum(state_map$sum_white_pop)
 
-View(county_map)
-write.csv(county_map,'county_map.cvs')
+black_ratio<- black_total_2016/28461107
+white_ratio<- white_total_2016/130697535
 
-url <- 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
-counties <- rjson::fromJSON(file=url)
-url2<- 'https://github.com/info-201a-wi22/a3-AZAHAZA/blob/main/source/urbanicity_map.cvs'
-data <- read.csv(url2, colClasses=c(county_name="character"))
+
+df2 <- state_map
+df2$hover <- with(df2, paste(state,"Aapi", sum_aapi_pop, "Black",sum_black_pop,"Latinx",sum_latinx_pop, 
+                            "Native",sum_native_pop, "White",sum_white_pop))
+View(df2)
+# give state boundaries a white border
+l <- list(color = toRGB("white"), width = 2)
+# specify some map projection/options
 g <- list(
   scope = 'usa',
   projection = list(type = 'albers usa'),
   showlakes = TRUE,
   lakecolor = toRGB('white')
 )
-fig2 <- plot_ly()
-fig2 <- fig2 %>% add_trace(
-  type="choropleth",
-  geojson=counties,
-  locations=data$county_name,
-  z=data$total_pop,
-  colorscale="Viridis",
-  zmin=0,
-  zmax=2535942,
-  marker=list(line=list(
-    width=0)
-  )
-)
-fig2 <- fig2 %>% colorbar(title = "US jail population")
-fig2 <- fig2 %>% layout(
-  title = "US location vs. jail population"
-)
 
+fig2 <- plot_geo(df2,locationmode = 'USA-states')
+fig2 <- fig2 %>% add_trace(
+  z = ~total_pop, text = ~hover, locations = ~state,
+  color = ~total_pop, colors = 'Blues'
+)
+fig2 <- fig2 %>% colorbar(title = "population count")
 fig2 <- fig2 %>% layout(
+  title = 'Us jail population and location count',
   geo = g
 )
 
